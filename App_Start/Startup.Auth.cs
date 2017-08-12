bffa8df1d527fe1ecc21c987719baac324b8fdfb
jerosidDev <30860256+jerosidDev@users.noreply.github.com>
2017-08-12 18:39:08 +0100

    Add files via upload

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+
+namespace Reporting_application.App_Start
+{
+
+    public static class MyAuthentication
+    {
+        public const string ApplicationCookie = "MyProjectAuthenticationType";
+
+    }
+
+
+    public partial class Startup
+    {
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+
+            // need to add UserManager into owin, because this is used in cookie invalidation
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = MyAuthentication.ApplicationCookie,
+                LoginPath = new PathString("/Login"),
+                Provider = new CookieAuthenticationProvider(),
+                CookieName = "MyCookieName",
+                CookieHttpOnly = true,
+                ExpireTimeSpan = TimeSpan.FromHours(12), // adjust to your needs
+            });
+
+
+        }
+
+    }
+
+
+}
---
+++ App_Start/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Reporting_application.App_Start.Startup))]
+
+namespace Reporting_application.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureAuth(app);
+        }
+    }
+}
