56b310b944654c6575db220186d007c266caaf66
jerosidDev <30860256+jerosidDev@users.noreply.github.com>
2017-08-16 11:49:17 +0100

    Create BkgAnalysisInfo.cs

---
+++ Repository/ThirdpartyDB/BkgAnalysisInfo.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Reporting_application.Repository.ThirdpartyDB
+{
+    public class BkgAnalysisInfo
+    {
+
+        public BkgAnalysisInfo()
+        {
+        }
+
+
+        public string Full_Reference { get; internal set; }
+
+        public string AgentName { get; internal set; }
+
+        public string EstimatedTurnover { get; internal set; }
+        public DateTime DateEntered { get; internal set; }
+        public DateTime DateConfirmed { get; internal set; }
+        public string DEPARTMENT { get; internal set; }
+        public string LocationName { get; internal set; }
+        public string BkgType { get; internal set; }
+        public string BookingStatus { get; internal set; }
+        public string ClientCategory { get; internal set; }
+        public string BDConsultant { get; internal set; }
+        public string Consultant { get; internal set; }
+        public string CompanyDepartment { get; internal set; }
+        public string PendingArea { get; internal set; }
+
+        public bool PastDeadline { get; internal set; }
+
+        public string DateConfirmedOnTP { get; internal set; }
+    }
+}
