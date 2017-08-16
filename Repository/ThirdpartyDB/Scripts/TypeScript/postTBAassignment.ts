a222b86768719808dcd75005c9ed207e1b09d6bb
jerosidDev <30860256+jerosidDev@users.noreply.github.com>
2017-08-16 12:27:39 +0100

    Create postTBAassignment.ts

---
+++ Repository/ThirdpartyDB/Scripts/TypeScript/postTBAassignment.ts
@@ -0,0 +1,52 @@
+/// <reference path="../typings/jquery/jquery.d.ts" />
+
+
+$(document).ready(function () {
+
+    $('#ShowLocationPanel+.panel .btn.btn-primary').click(function () {
+        SendAjaxPost(this);
+    });
+
+});
+
+function SendAjaxPost(button: any) {
+
+    // get the preceding select
+    let select: any = $(button).prev('select');
+    let selectOption: any = select.find(':selected');
+
+    // get the preceding label
+    let row = $(button).parent();
+    let label: any = row.find('label');
+
+    let tbaLoc: TBALocation = { CODE: label.attr('value'), NAME: label.text(), Csl_Assigned_INITIAL: selectOption.val() };
+    let cc: ContractConsultant = { INITIALS: selectOption.val(), NAME: selectOption.text(), LocationsAssigned: [tbaLoc] };
+
+    if (cc.INITIALS == undefined) return;
+
+    let urlAction: string = "/PerformanceOverview/PostTBAsInformation";
+    let jqxhr: JQueryXHR = $.post(urlAction, cc);
+    jqxhr.done(function () {
+        // if change successful, change the color to green and the btn text to "Saved"
+        select.css({ "color": "black", "background-color": "white" });
+        $(button).text("Saved");
+    });
+
+
+}
+
+
+
+
+class ContractConsultant {
+    INITIALS: string;
+    NAME: string;
+    LocationsAssigned: TBALocation[];
+}
+
+
+class TBALocation {
+    CODE: string;
+    NAME: string;
+    Csl_Assigned_INITIAL: string;
+}
